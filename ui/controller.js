6bc08d1ad23dd96851dfee43066787b331c6ebfb
Shon <shon.saoji@gmail.com>
2013-01-05 18:08:57 -0800

    ERror classes for sending email

--- ui/controller.js
+++ ui/controller.js
@@ -2,10 +2,31 @@ $("#contactBtn").click(function(e) {
 	var sender_name = $("#senderName").val();
 	var sender_email = $("#senderEmail").val();
 	var sender_msg = $("#senderMsg").val();
+	ret = false;
+	if(sender_name.length == 0) {
+		$("#senderName").addClass("error");
+		ret = true;
+	} 
+	
+	if ( sender_email.length == 0 ) {
+		$("#senderEmail").addClass("error");
+		ret = true;
+	} 
+	
+	if (sender_msg.length ==0 ) {
+		$("#senderMsg").addClass("error");
+		ret = true;
+	}
+	if(ret) {
+		return;
+	} 
 	$.get("/send_mail.php?name=" + sender_name + "&email=" + sender_email + "&msg=" + sender_msg, function() {
 		$("#senderName").val("");
 		$("#senderEmail").val("");
 		$("#senderMsg").val("");
+		$("#senderName").removeClass("error");
+		$("#senderEmail").removeClass("error");
+		$("#senderMsg").removeClass("error");
 		alert('Thank you for the enquiry. Your message has been sent!');
 	});
 });
