f5ba53eb60cda1b781d35464bb01a850bee44f68
Shon <shon.saoji@gmail.com>
2013-01-05 17:38:27 -0800

    Email Code

--- ui/controller.js
+++ ui/controller.js
@@ -1,6 +1,10 @@
 $("#contactBtn").click(function(e) {
-	var sender_name = $("#senderName");
-	var sender_email = $("#senderEmail");
-	var sender_msg = $("#senderMsg");
-	$.get("/send_mail.php?")
+	var sender_name = $("#senderName").val();
+	var sender_email = $("#senderEmail").val();
+	var sender_msg = $("#senderMsg").val();
+	$.get("/send_mail.php?name=" + sender_name + "&email=" + sender_email + "&msg=" + sender_msg, function() {
+		$("#senderName").val("");
+		$("#senderEmail").val("");
+		$("#senderMsg").val("");
+	});
 });
