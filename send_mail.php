165aea3dcb3995441281ce41f0b0be3f23572bce
Shon <shon.saoji@gmail.com>
2013-01-05 17:43:52 -0800

    Email From field added

--- send_mail.php
+++ send_mail.php
@@ -1,10 +1,11 @@
 <?php
 $name = $_GET['name'];
 $email = $_GET['email'];
-$msg = "You have received an email from $name, email = $email. The message is <br />";
+$msg = "You have received an email from $name, email = $email. The message is \n";
 $msg .= $_GET['msg'];
+$headers = 'From: admin@cerebrateinc.com';
 
 
 $to = "shon.saoji@gmail.com";
-mail($email, "Enquiry for cerebrate", $msg);
+mail($email, "Enquiry for cerebrate", $msg, $headers);
 
